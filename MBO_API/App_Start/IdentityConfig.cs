747afe847f0be415a207c118eab5271e6c810300
mubaidr <mubaidr@gmail.com>
2016-12-13 10:34:29 +0500

    simplify password requirements

--- MBO_API/App_Start/IdentityConfig.cs
+++ MBO_API/App_Start/IdentityConfig.cs
@@ -29,10 +29,10 @@ namespace MBO_API
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
             var dataProtectionProvider = options.DataProtectionProvider;
             if (dataProtectionProvider != null)
