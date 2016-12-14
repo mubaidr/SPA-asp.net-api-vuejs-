04604ce49a6304f963ca155e6f62522106e2dfee
Muhammad Ubaid Raza <mubaidr@gmail.com>
2016-12-14 21:59:59 +0500

    WIP: Validation message

--- MBO/client/components/Account/Signup.vue
+++ MBO/client/components/Account/Signup.vue
@@ -4,27 +4,29 @@
       <div class="col-md-offset-4 col-md-4">
         <h1>Signup</h1>
         <p>Please provide following details to signup.</p>
-        <div v-show="$v.validationGroup.$invalid" class="text-danger">
+        <div v-show="formValidation.isInvalid" class="text-danger">
           <p>Please fix following issues and then try to submit!</p>
           <ul class="validation-list">
-            {{validationMessage}}
+            <pre>{{formValidation.message}}</pre>
           </ul>
         </div>
-        <div class="form-group" v-bind:class="{ 'has-error': $v.username.$error }">
-          <label for="Username" class="control-label">Username</label>
-          <input v-model="username" type="text" class="form-control" placeholder="Username" v-model.trim="username" @input="$v.username.$touch()">
+        <div class="form-group" v-bind:class="{ 'has-error': $v.credentials.username.$error }">
+          <label for="username" class="control-label">Username</label>
+          <input v-model="credentials.username" type="text" name="username" class="form-control" placeholder="Username" v-model.trim="credentials.username"
+            @input="$v.credentials.username.$touch()">
         </div>
-        <div class="form-group" v-bind:class="{ 'has-error': $v.password.$error }">
-          <label for="Password" class="control-label">Password</label>
-          <input v-model="password" type="password" class="form-control" placeholder="Password" v-model.trim="password" @input="$v.password.$touch()">
+        <div class="form-group" v-bind:class="{ 'has-error': $v.credentials.password.$error }">
+          <label for="password" class="control-label">Password</label>
+          <input v-model="credentials.password" type="password" name="password" class="form-control" placeholder="Password" v-model.trim="credentials.password"
+            @input="$v.credentials.password.$touch()">
         </div>
-        <div class="form-group" v-bind:class="{ 'has-error': $v.confirmPassword.$error }">
-          <label for="ConfirmPassword" class="control-label">Confirm Password</label>
-          <input v-model="confirmPassword" type="password" class="form-control" placeholder="Confirm Password" v-model.trim="confirmPassword"
-            @input="$v.confirmPassword.$touch()">
+        <div class="form-group" v-bind:class="{ 'has-error': $v.credentials.confirmPassword.$error }">
+          <label for="confirmPassword" class="control-label">Confirm Password</label>
+          <input v-model="credentials.confirmPassword" type="password" name="confirmPassword" class="form-control" placeholder="Confirm Password"
+            v-model.trim="credentials.confirmPassword" @input="$v.credentials.confirmPassword.$touch()">
         </div>
         <div class="form-group">
-          <button v-on:click="register" class="btn btn-block btn-primary" v-bind:disabled="$v.validationGroup.$invalid || !$v.validationGroup.$dirty">Create Account</button>
+          <button v-on:click="register" class="btn btn-block btn-primary" v-bind:disabled="$v.validationGroup.$invalid">Create Account</button>
         </div>
         <div class="form-group">
           Already have an account?
@@ -34,7 +36,7 @@
         </div>
       </div>
     </div>
-    <div v-show="loading" class="backdrop">
+    <div v-show="progress.loading" class="backdrop">
       <ring-loader></ring-loader>
     </div>
   </div>
@@ -50,55 +52,68 @@
   import RingLoader from 'vue-spinner/src/RingLoader'
 
   export default {
-    data: function () {
-      return {
-        username: 'tester',
-        password: 'tester1234',
-        confirmPassword: 'tester1234',
-        loading: false,
-        successMessage: ''
-      }
-    },
     components: {
       RingLoader
     },
-    computed: {
-      validationMessage: function () {
-        return this.$v.validationGroup;
+    data: function () {
+      return {
+        credentials: {
+          username: '',
+          password: '',
+          confirmPassword: ''
+        },
+        progress: {
+          loading: false,
+          successMessage: ''
+        }
       }
     },
     validations: {
-      username: {
-        required,
-        alphaNum,
-        minLength: minLength(6)
+      credentials: {
+        username: {
+          required,
+          alphaNum,
+          minLength: minLength(6)
+        },
+        password: {
+          required,
+          alphaNum,
+          minLength: minLength(6)
+        },
+        confirmPassword: {
+          required,
+          sameAs: sameAs('password')
+        }
       },
-      password: {
-        required,
-        alphaNum,
-        minLength: minLength(6)
-      },
-      confirmPassword: {
-        required,
-        sameAs: sameAs('password')
-      },
-      validationGroup: ['username', 'password', 'confirmPassword']
+      validationGroup: ['credentials.username', 'credentials.password', 'credentials.confirmPassword']
+    },
+    computed: {
+      formValidation: function () {
+        return {
+          isInvalid: true,
+          message: this.$v.validationGroup
+        }
+      }
     },
     methods: {
       spinner: function (bool) {
         this.loading = bool;
       },
       register: function (event) {
-        var _self = this;
         event.preventDefault();
-        _self.spinner(true);
-        axios.get('http://localhost:8080/signup').then(function (res) {
-          console.log(res);
-          _self.spinner(false);
-        }).catch(function (err) {
-          console.log(err);
-          _self.spinner(false);
-        });
+        var _self = this;
+        if (_self.$v.validationGroup) {
+          _self.spinner(true);
+          axios.get('http://localhost:8080/signup').then(function (res) {
+            console.log(res);
+            _self.spinner(false);
+          }).catch(function (err) {
+            console.log(err);
+            _self.spinner(false);
+          });
+        } else {
+          alert('Not valid form');
+        }
       }
     }
   }
