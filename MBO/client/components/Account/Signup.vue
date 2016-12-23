7710b10d975fbf845fd79fa749fbd830c5343be5
mubaidr <mubaidr@gmail.com>
2016-12-23 15:40:29 +0500

    disable form during submit

--- MBO/client/components/Account/Signup.vue
+++ MBO/client/components/Account/Signup.vue
@@ -13,35 +13,35 @@
             <div class="md-title">Create an account</div>
             <div class="md-subhead">Please provide required information.</div>
           </md-card-header-text>
-          <md-spinner md-indeterminate class="md-accent" v-show="progress.loading"></md-spinner>
+          <md-spinner md-indeterminate class="md-accent" v-show="status.loading"></md-spinner>
         </md-card-header>
         <md-card-content>
           <md-input-container :class="{'md-input-invalid': errors.has('email')}">
             <label>Username</label>
-            <md-input v-model="credentials.email" type="email" v-model.trim="credentials.email" v-validate.initial="credentials.email"
-              data-vv-rules="required|alpha|min:5"></md-input>
+            <md-input v-model="credentials.email" type="email" v-model.trim="credentials.email" v-validate="credentials.email" data-vv-rules="required|alpha|min:5"
+              :disabled="status.loading"></md-input>
             <span class="md-error">{{errors.first('email')}}</div>
           </md-input-container>
           <md-input-container md-has-password :class="{'md-input-invalid': errors.has('password')}">
             <label>Password</label>
-            <md-input v-model="credentials.password" type="password" v-model.trim="credentials.password" v-validate.initial="credentials.password" data-vv-rules="required|alpha|min:5"></md-input>
+            <md-input v-model="credentials.password" type="password" v-model.trim="credentials.password" v-validate="credentials.password" data-vv-rules="required|alpha|min:5" :disabled="status.loading"></md-input>
             <span class="md-error">{{errors.first('password')}}</div>
           </md-input-container>
           <md-input-container md-has-password :class="{'md-input-invalid': errors.has('confirmPassword')}">
             <label>Confirm Password</label>
-            <md-input v-model="credentials.confirmPassword" type="password" v-model.trim="credentials.confirmPassword" v-validate.initial="credentials.confirmPassword" data-vv-rules="required|alpha|min:5"></md-input>
+            <md-input v-model="credentials.confirmPassword" type="password" v-model.trim="credentials.confirmPassword" v-validate.initial="credentials.confirmPassword" data-vv-rules="required|alpha|min:5" :disabled="status.loading"></md-input>
             <span class="md-error">{{errors.first('confirmPassword')}}</div>
           </md-input-container>
         </md-card-content>
         <md-card-actions>
           <router-link tag="md-button" to="/signin" class="md-accent">Already have an account?</router-link>
-          <md-button class="md-raised md-accent" @click="signup">Register</md-button>
+          <md-button class="md-raised md-accent" @click="signup" :disabled="status.loading">Register</md-button>
         </md-card-actions>
         <md-card-content>
-          <div v-show="progress.statusMessage" class="alert alert-danger">
-            {{progress.statusMessage}}
+          <div v-show="status.message" class="red">
+            {{status.message}}
           </div>
-          <pre>{{errors.has('email')}}</pre>
+          <pre>{{errors.collect()}}</pre>
         </md-card-content>
       </md-card>
     </md-layout>
@@ -61,38 +61,33 @@
           email: '',
           password: '',
           confirmPassword: ''
-        },
-        progress: {
+        },        
+        status: {
           loading: false,
-          statusMessage: ''
+          message: ''
         }
       }
-    },
-    computed: {
-
     },
     methods: {
       signup: function (event) {
         event.preventDefault();
         var _self = this;
-        /*
-        //_self.$v.$touch();
-
-        if (_self.$v.$invalid) {
+        if (_self.errors.length) {
           return false;
         } else {
-
+            _self.status.loading = true;
           signup({
             Email: this.credentials.email,
             Password: this.credentials.password,
             ConfirmPassword: this.credentials.confirmPassword
           }).then(function (res) {
+            _self.status.loading = false;
             console.log(res);
           }).catch(function (err) {
+            _self.status.loading = false;
             console.log(err);
           });
-
-      }*/
+      }
       }
     },
     ready: function () {
