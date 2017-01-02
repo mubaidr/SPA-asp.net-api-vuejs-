9e7df70a39a6690c238f3106cf276e3de09fdc18
mubaidr <mubaidr@gmail.com>
2017-01-02 15:30:13 +0500

    tweaks

--- MBO/client/services/tasks/index.js
+++ MBO/client/services/tasks/index.js
@@ -8,18 +8,20 @@ import api from 'api';
 import axios from 'utilities/axios';
 import qs from 'qs';
 
+const path = "/api/MainTasks/";
+
 function create(obj) {
-  return axios.post(api.url + '/api/tasks/', obj);
+  return axios.post(api.url + path, obj);
 }
 
 function edit(obj) {
-  return axios.post(api.url + '/api/tasks/', obj);
+  return axios.post(api.url + path, obj);
 }
 
 function remove(obj) {
-  return axios.post(api.url + '/api/tasks/', obj);
+  return axios.post(api.url + path, obj);
 }
 
 function list(obj) {
-  return axios.post(api.url + '/api/tasks/', obj);
+  return axios.post(api.url + path, obj);
 }
