2848e11f91ba8aa41ffefd0c636ca67be8a11dd6
mubaidr <mubaidr@gmail.com>
2017-04-10 15:48:26 +0500

    Add contact sidebar

--- MBO/src/components/Management/Messages/Inbox.vue
+++ MBO/src/components/Management/Messages/Inbox.vue
@@ -1,33 +1,43 @@
 <template>
   <div>
     <!--<md-whiteframe md-tag="section" class="full-width">-->
-      <md-toolbar class="md-transparent md-dense">
-        <md-button class="md-icon-button md-accent" @click.native="$router.push({ path: '/messages' })">
-          <md-tooltip md-direction="top">Inbox</md-tooltip>
-          <md-icon>arrow_back</md-icon>
-        </md-button>
-        <span>Send Message</span>
-        <span style="flex: 1"></span>
-        <md-button class="md-accent" @click.native="$router.push({path: '/messages/trash'})">
-          <md-tooltip md-direction="top">Archived</md-tooltip>
-          <md-icon>delete</md-icon>
-          Trash
-        </md-button>
-        <md-button class="md-accent" @click.native="$router.push({path: '/tasks'})">
-          <md-tooltip md-direction="top">Tasks</md-tooltip>
-          <md-icon>assignment</md-icon>
-          Tasks
-        </md-button>
-        <md-button class="md-accent" @click.native="$router.push({path: '/dashboard'})">
-          <md-tooltip md-direction="top">Dashboard</md-tooltip>
-          <md-icon>dashboard</md-icon>
-          Dashboard
-        </md-button>
-      </md-toolbar>
+    <md-toolbar class="md-transparent md-dense">
+      <md-button class="md-icon-button md-accent" @click.native="$router.push({ path: '/messages' })">
+        <md-tooltip md-direction="top">Inbox</md-tooltip>
+        <md-icon>arrow_back</md-icon>
+      </md-button>
+      <span>Send Message</span>
+      <span style="flex: 1"></span>
+      <md-button class="md-accent" @click.native="$router.push({path: '/messages/trash'})">
+        <md-tooltip md-direction="top">Archived</md-tooltip>
+        <md-icon>delete</md-icon>
+        Trash
+      </md-button>
+      <md-button class="md-accent" @click.native="$router.push({path: '/tasks'})">
+        <md-tooltip md-direction="top">Tasks</md-tooltip>
+        <md-icon>assignment</md-icon>
+        Tasks
+      </md-button>
+      <md-button class="md-accent" @click.native="$router.push({path: '/dashboard'})">
+        <md-tooltip md-direction="top">Dashboard</md-tooltip>
+        <md-icon>dashboard</md-icon>
+        Dashboard
+      </md-button>
+    </md-toolbar>
     <!--</md-whiteframe>-->
     <br/>
     <md-layout>
-Another thing
+      <md-layout md-flex-small="33" md-flex="20">
+        <div class="contact-list">
+          <md-button v-for="user in Catalog.Users" class="full-width text-left" @click.native="openChat(user)" :class="{'active' : user.Id == ActiveUser.Id}" :disabled="user.Id == ActiveUser.Id">
+            <div class="md-list-text-container">
+              <span>{{user.Email}}</span>
+            </div>
+          </md-button>
+        </div>
+      </md-layout>
+      <md-layout></md-layout>
+      <md-layout></md-layout>
     </md-layout>
   </div>
 </template>
@@ -43,13 +53,19 @@ Another thing
         Page: {
           isLoading: false
         },
-        Users: [],
+        ActiveUser: [],
         Catalog: {
           Users: []
         }
       }
     },
-    methods: {},
+    computed: {},
+    methods: {
+      openChat (user) {
+        var _self = this
+        _self.$set(_self, 'ActiveUser', user)
+      }
+    },
     mounted () {
       const _self = this
 
@@ -60,5 +76,22 @@ Another thing
       })
     }
   }
+
 </script>
-<style></style>
+<style scoped>
+  .contact-list button{
+    background-color: #eee;
+    margin: 0;
+    padding: 10px;
+    border-bottom:1px solid #fff;
+  }
+
+  .contact-list button:hover{
+    color: #9c27b0;
+  }
+
+  .contact-list button.active{
+    color: white;
+    background-color:#9c27b0;
+  }
+</style>
