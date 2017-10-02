791f6cbb162f30aa94ae1653ed4315736f7b0fb7
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-10-02 13:01:43 +0500

    remove unsuded files

--- MBO/src/utilities/router/index.js
+++ MBO/src/utilities/router/index.js
@@ -15,8 +15,6 @@ import Signup from 'components/Account/Signup'
 import Profile from 'components/Account/Profile'
 // Data
 import Dashboard from 'components/Management/Dashboard.vue'
-import Settings from 'components/Management/Settings.vue'
-import Search from 'components/Management/Search.vue'
 // Tasks
 import Tasks from 'components/Management/Tasks/index.vue'
 import TasksList from 'components/Management/Tasks/List.vue'
@@ -33,100 +31,88 @@ const router = new Router({
   mode: 'history',
   root: '/',
   routes: [{
-    path: '/',
-    component: Home
-  }, {
-    path: '/home',
-    redirect: '/'
-  }, {
-    path: '/welcome',
-    meta: {
-      requiresAuth: true
-    }
-  }, {
-    path: '/about',
-    component: About
-  }, {
-    path: '/contact',
-    component: Contact
-  }, {
-    path: '/signup',
-    component: Signup
-  }, {
-    path: '/signin',
-    component: Signin
-  }, {
-    path: '/signout'
-  }, {
-    path: '/dashboard',
-    component: Dashboard,
-    meta: {
-      requiresAuth: true
-    }
-  }, {
-    path: '/settings',
-    component: Settings,
-    meta: {
-      requiresAuth: true
-    }
-  }, {
-    path: '/profile',
-    component: Profile,
-    meta: {
-      requiresAuth: true
-    }
-  }, {
-    path: '/search',
-    component: Search,
-    meta: {
-      requiresAuth: true
-    }
-  }, {
-    path: '/error',
-    component: Error_,
-    children: [{
-      path: '404',
-      component: Error404
+      path: '/',
+      component: Home
     }, {
-      path: '500',
-      component: Error500
-    }]
-  }, {
-    // Tasks
-    path: '/tasks',
-    component: Tasks,
-    meta: {
-      requiresAuth: true
+      path: '/home',
+      redirect: '/'
+    }, {
+      path: '/welcome',
+      meta: {
+        requiresAuth: true
+      }
+    }, {
+      path: '/about',
+      component: About
+    }, {
+      path: '/contact',
+      component: Contact
+    }, {
+      path: '/signup',
+      component: Signup
+    }, {
+      path: '/signin',
+      component: Signin
+    }, {
+      path: '/signout'
+    }, {
+      path: '/dashboard',
+      component: Dashboard,
+      meta: {
+        requiresAuth: true
+      }
+    }, {
+      path: '/profile',
+      component: Profile,
+      meta: {
+        requiresAuth: true
+      }
+    }, {
+      path: '/error',
+      component: Error_,
+      children: [{
+        path: '404',
+        component: Error404
+      }, {
+        path: '500',
+        component: Error500
+      }]
+    }, {
+      // Tasks
+      path: '/tasks',
+      component: Tasks,
+      meta: {
+        requiresAuth: true
+      },
+      children: [{
+        path: '',
+        component: TasksList
+      }, {
+        path: 'trash',
+        component: TasksTrash
+      }, {
+        path: 'create',
+        component: TasksCreate
+      }, {
+        name: 'task-details',
+        path: 'details',
+        component: TasksDetails
+      }]
     },
-    children: [{
-      path: '',
-      component: TasksList
+    {
+      path: '/messages',
+      component: Messages,
+      meta: {
+        requiresAuth: true
+      },
+      children: [{
+        path: '',
+        component: MessagesInbox
+      }]
     }, {
-      path: 'trash',
-      component: TasksTrash
-    }, {
-      path: 'create',
-      component: TasksCreate
-    }, {
-      name: 'task-details',
-      path: 'details',
-      component: TasksDetails
-    }]
-  },
-  {
-    path: '/messages',
-    component: Messages,
-    meta: {
-      requiresAuth: true
-    },
-    children: [{
-      path: '',
-      component: MessagesInbox
-    }]
-  }, {
-    path: '*',
-    redirect: '/error/404'
-  }
+      path: '*',
+      redirect: '/error/404'
+    }
   ]
 })
 
